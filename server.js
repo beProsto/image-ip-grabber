b35caad15efe92ed6e9a81ea02c2d2c468cc47ed
beProsto <lavionperavion2@gmail.com>
2022-02-04 23:01:13 +0100

    smil.png

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const Jimp = require('jimp')
+
+let image;
+Jimp.read('./smil.jpg')
+.then(img => {
+	image = img;
+	console.log("Image loaded!");
+})
+.catch(err => {
+	console.warn("\n!!! IMAGE COULDN'T BE READ !!!\n");
+	console.error(err);
+});
+
+let font;
+Jimp.loadFont(Jimp.FONT_SANS_16_BLACK)
+.then(fnt => {
+	font = fnt;
+	console.log("Font loaded!");
+})
+.catch(err => {
+	console.warn("\n!!! FONT COULDN'T BE READ !!!\n");
+	console.error(err);
+});
+
+const requestListener = async (req, res) => {
+	// console.log("req from " + req.headers['x-forwarded-for']);
+	const img = image.clone();
+
+	img.print(font, 4, 132, req.socket.remoteAddress);
+
+	res.setHeader("Content-Type", "image/jpg"); 
+	res.writeHead(200);
+	res.end(await img.getBufferAsync(Jimp.MIME_JPEG));
+}
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
